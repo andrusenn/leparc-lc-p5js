6dea784e36b71b2605fc06d103458c414b5c791f
andru <andres.senn@gmail.com>
2019-03-25 11:08:52 -0300

    - ejemplos

--- src/duchamplc-resources/libs/duchamp/duchamp.js
+++ src/duchamplc-resources/libs/duchamp/duchamp.js
@@ -1,12 +1,30 @@
-class Duchamp{
-      contructor(){}
-      img(x,y){
-            loadImage(libPath('duchamp') + 'roto.png',(img)=>{
-                  push()
-                  imageMode(CENTER)
-                  image(img,x,y)
-                  pop()
+/**
+ * Duchamp!!
+ * [ES] 
+ *    Clase de ejemplo para la carga dinamica de codigo externo
+ *    Imagen: rotorelief 1 (Marcel Duchamp)
+ * [EN]
+ *    Example class for load external code dynamically
+ *    Image: rotorelief 1 (Marcel Duchamp)
+ * 
+ * 
+ */
+
+class Duchamp {
+      constructor() {
+            this.i = null
+            loadImage(libPath('duchamp') + 'rotorelief.png', (i) => {
+                  this.i = i
             })
       }
+      rotorelief(x, y) {
+            if (this.i) {
+                  push()
+                  imageMode(CENTER)
+                  image(this.i, x, y)
+                  pop()
+            }
+      }
 }
-dlc = new Duchamp()
+// Igual a la libreria
+duchamp = new Duchamp()
