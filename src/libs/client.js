d41e5f52bd8d7ba553a7cf081591525b2ecbb05b
andres.senn@gmail.com <andres.senn@gmail.com>
2019-03-29 17:52:01 -0300

    - texto encabezado

--- src/libs/client.js
+++ src/libs/client.js
@@ -1,44 +1,51 @@
+/**
+ * Script client.js
+ * 
+ * Se encarga de administrar
+ * el envío de datos y acciones al servidos
+ * 
+ */
 let socket = null;
-let dp5
+let lp5
 // let cursor = { line: 0, ch: 0 }
-function connect(_dp5) {
-      dp5 = _dp5
-      socket = io('http://' + dp5.configs['server-ip'] + ':' + dp5.configs['port']);
+function connect(_lp5) {
+      lp5 = _lp5
+      socket = io('http://' + lp5.configs['server-ip'] + ':' + lp5.configs['port']);
       // recive desde servidor
       socket.on('liveleparc1', data => {
             //console.log('recive servidor', 'codigo')
-            dp5.cmSetup.setValue(data.codeSetup)
-            dp5.cmDraw.setValue(data.codeDraw)
-            dp5.cmAux.setValue(data.codeAux)
+            lp5.cmSetup.setValue(data.codeSetup)
+            lp5.cmDraw.setValue(data.codeDraw)
+            lp5.cmAux.setValue(data.codeAux)
             // sincroniza
-            if(Dp5.sync) frameCount = data.frameSync
+            if(Lp5.sync) frameCount = data.frameSync
             // Restaurar cursor local despues de la actualizacion
-            dp5.restoreCursor(dp5.cmSetup, dp5.cmSetupCp)
-            dp5.restoreCursor(dp5.cmDraw, dp5.cmDrawCp)
-            dp5.restoreCursor(dp5.cmAux, dp5.cmAuxCp)
+            lp5.restoreCursor(lp5.cmSetup, lp5.cmSetupCp)
+            lp5.restoreCursor(lp5.cmDraw, lp5.cmDrawCp)
+            lp5.restoreCursor(lp5.cmAux, lp5.cmAuxCp)
       })
       socket.on('broadcast', data => {
             //console.log('recive servidor', 'codigo')
-            dp5.cmSetup.setValue(data.codeSetup)
-            dp5.cmDraw.setValue(data.codeDraw)
-            dp5.cmAux.setValue(data.codeAux)
+            lp5.cmSetup.setValue(data.codeSetup)
+            lp5.cmDraw.setValue(data.codeDraw)
+            lp5.cmAux.setValue(data.codeAux)
             // Restaurar cursor local despues de la actualizacion
-            dp5.restoreCursor(dp5.cmSetup, dp5.cmSetupCp)
-            dp5.restoreCursor(dp5.cmDraw, dp5.cmDrawCp)
-            dp5.restoreCursor(dp5.cmAux, dp5.cmAuxCp)
+            lp5.restoreCursor(lp5.cmSetup, lp5.cmSetupCp)
+            lp5.restoreCursor(lp5.cmDraw, lp5.cmDrawCp)
+            lp5.restoreCursor(lp5.cmAux, lp5.cmAuxCp)
       })
       socket.on('eval', eval => {
-            if(eval == 'draw') dp5.evalDraw()
-            if(eval == 'setup') dp5.evalSetup()
-            if(eval == 'aux') dp5.evalAux()
+            if(eval == 'draw') lp5.evalDraw()
+            if(eval == 'setup') lp5.evalSetup()
+            if(eval == 'aux') lp5.evalAux()
       })
       // Sincroniza frameCount con servidor
       socket.on('sync', fc => {
-            if(Dp5.sync) frameCount = fc
+            if(Lp5.sync) frameCount = fc
       })
 }
-exports.connect = function (dp5) {
-      connect(dp5)
+exports.connect = function (lp5) {
+      connect(lp5)
 }
 exports.close = function () {
       socket.close()
@@ -48,23 +55,23 @@ exports.eval = function (_b) {
 }
 exports.sendServer = function () {
       // cursores 
-      let cSetup = dp5.cmSetup.getCursor()
-      let cDraw = dp5.cmDraw.getCursor()
-      let cAux = dp5.cmAux.getCursor()
+      let cSetup = lp5.cmSetup.getCursor()
+      let cDraw = lp5.cmDraw.getCursor()
+      let cAux = lp5.cmAux.getCursor()
 
       let o = {
             // ID
             id: socket.id,
-            cmContext: dp5.cmFocused,
+            cmContext: lp5.cmFocused,
             // bloques
-            codeSetup: dp5.cmSetup.getValue(),
-            codeDraw: dp5.cmDraw.getValue(),
-            codeAux: dp5.cmAux.getValue(),
+            codeSetup: lp5.cmSetup.getValue(),
+            codeDraw: lp5.cmDraw.getValue(),
+            codeAux: lp5.cmAux.getValue(),
             // cursores
             cSetup: cSetup,
             cDraw: cDraw,
             cAux: cAux,
-            nodeName: Dp5.nodeName
+            nodeName: Lp5.nodeName
       }
       // if (socket) {
       socket.emit('liveleparc1', o)
--- src/libs/server.js
+++ src/libs/server.js
@@ -1,3 +1,11 @@
+/**
+ * Script server.js
+ * 
+ * Se encarga de administrar
+ * los clientes conectados para el envio de datos y
+ * acciones
+ * 
+ */
 const path = require('path')
 const express = require('express');
 const app = express();
@@ -5,13 +13,13 @@ const SocketIo = require('socket.io')
 
 // inicia el servidor
 let sio
-let dp5
+let lp5
 let server
-function initServer(_dp5) {
-      dp5 = _dp5
+function initServer(_lp5) {
+      lp5 = _lp5
 
       // Settings
-      app.set('port', dp5.configs['port'])
+      app.set('port', lp5.configs['port'])
       app.use(express.static(path.join(__dirname, '/')))
 
       server = app.listen(app.get('port'), () => {
@@ -22,17 +30,17 @@ function initServer(_dp5) {
       // websockets
       sio.sockets.on('connection', socket => {
             // Marcadores------------------------------------------------------
-            dp5.markers[socket.id.toString()] = {
+            lp5.markers[socket.id.toString()] = {
                   el: document.createElement('span')
             }
 
-            dp5.markers[socket.id.toString()].el.setAttribute('class', 'client-cursor')
-            dp5.markers[socket.id.toString()].el.setAttribute('id', socket.id.toString())
-            // dp5.markers[socket.id.toString()].el.innerHTML = '<span class="label">' + socket.id.toString() + '</span>'
+            lp5.markers[socket.id.toString()].el.setAttribute('class', 'client-cursor')
+            lp5.markers[socket.id.toString()].el.setAttribute('id', socket.id.toString())
+            // lp5.markers[socket.id.toString()].el.innerHTML = '<span class="label">' + socket.id.toString() + '</span>'
 
             socket.on('disconnect', () => {
                   // Remueve marcador
-                  dp5.markers[socket.id.toString()] = null
+                  lp5.markers[socket.id.toString()] = null
                   var el = document.getElementById(socket.id.toString());
                   el.parentElement.removeChild(el);
             })
@@ -41,38 +49,38 @@ function initServer(_dp5) {
             // envia codigo del servidor
             let o = {
                   frameSync: frameCount,
-                  codeSetup: dp5.cmSetup.getValue(),
-                  codeDraw: dp5.cmDraw.getValue(),
-                  codeAux: dp5.cmAux.getValue()
+                  codeSetup: lp5.cmSetup.getValue(),
+                  codeDraw: lp5.cmDraw.getValue(),
+                  codeAux: lp5.cmAux.getValue()
             }
             socket.broadcast.emit('broadcast', o)
             //----------------------------------------
 
             // recive desde cliente
             socket.on('liveleparc1', data => {
-                  dp5.cmSetup.setValue(data.codeSetup)
-                  dp5.cmDraw.setValue(data.codeDraw)
-                  dp5.cmAux.setValue(data.codeAux)
+                  lp5.cmSetup.setValue(data.codeSetup)
+                  lp5.cmDraw.setValue(data.codeDraw)
+                  lp5.cmAux.setValue(data.codeAux)
 
                   // Restaurar cursor local despues de la actualizacion
-                  dp5.restoreCursor(dp5.cmSetup, dp5.cmSetupCp)
-                  dp5.restoreCursor(dp5.cmDraw, dp5.cmDrawCp)
-                  dp5.restoreCursor(dp5.cmAux, dp5.cmAuxCp)
+                  lp5.restoreCursor(lp5.cmSetup, lp5.cmSetupCp)
+                  lp5.restoreCursor(lp5.cmDraw, lp5.cmDrawCp)
+                  lp5.restoreCursor(lp5.cmAux, lp5.cmAuxCp)
 
                   // Actualiza marcadores -----------------------------------
                   let label = (data.nodeName != '') ? data.nodeName : data.id
-                  dp5.markers[data.id.toString()].el.innerHTML = '<span class="label">' + label + '</span>'
+                  lp5.markers[data.id.toString()].el.innerHTML = '<span class="label">' + label + '</span>'
                   //console.log(data.cmContext)
                   if (data.cmContext) {
-                        if (data.cmContext == 'draw') dp5.cmDraw.setBookmark(data.cDraw, { widget: dp5.markers[data.id.toString()].el })
-                        if (data.cmContext == 'setup') dp5.cmSetup.setBookmark(data.cSetup, { widget: dp5.markers[data.id.toString()].el })
-                        if (data.cmContext == 'aux') dp5.cmAux.setBookmark(data.cAux, { widget: dp5.markers[data.id.toString()].el })
+                        if (data.cmContext == 'draw') lp5.cmDraw.setBookmark(data.cDraw, { widget: lp5.markers[data.id.toString()].el })
+                        if (data.cmContext == 'setup') lp5.cmSetup.setBookmark(data.cSetup, { widget: lp5.markers[data.id.toString()].el })
+                        if (data.cmContext == 'aux') lp5.cmAux.setBookmark(data.cAux, { widget: lp5.markers[data.id.toString()].el })
                   }
                   // Reenvia la actualizacion a clientes
                   let o = {
                         // ID
                         id: socket.id,
-                        cmContext: dp5.cmFocused,
+                        cmContext: lp5.cmFocused,
                         frameSync: frameCount,
                         codeSetup: data.codeSetup,
                         codeDraw: data.codeDraw,
@@ -83,9 +91,9 @@ function initServer(_dp5) {
             // Eval
 
             socket.on('eval', eval => {
-                  if (eval == 'draw') dp5.evalDraw()
-                  if (eval == 'setup') dp5.evalSetup()
-                  if (eval == 'aux') dp5.evalAux()
+                  if (eval == 'draw') lp5.evalDraw()
+                  if (eval == 'setup') lp5.evalSetup()
+                  if (eval == 'aux') lp5.evalAux()
                   // Reenvia la actualizacion a clientes
                   socket.broadcast.emit('eval', eval)
             })
@@ -107,27 +115,27 @@ exports.eval = function (_b) {
 exports.close = function () {
       server.close()
 }
-exports.initServer = function (dp5) {
-      initServer(dp5)
+exports.initServer = function (lp5) {
+      initServer(lp5)
 }
 exports.sendClient = function (fc) {
       // envia a cliente    
-      let cSetup = dp5.cmSetup.getCursor()
-      let cDraw = dp5.cmDraw.getCursor()
-      let cAux = dp5.cmAux.getCursor()
+      let cSetup = lp5.cmSetup.getCursor()
+      let cDraw = lp5.cmDraw.getCursor()
+      let cAux = lp5.cmAux.getCursor()
 
       let o = {
-            cmContext: dp5.cmFocused,
+            cmContext: lp5.cmFocused,
             //
             frameSync: fc,
-            codeSetup: dp5.cmSetup.getValue(),
-            codeDraw: dp5.cmDraw.getValue(),
-            codeAux: dp5.cmAux.getValue(),
+            codeSetup: lp5.cmSetup.getValue(),
+            codeDraw: lp5.cmDraw.getValue(),
+            codeAux: lp5.cmAux.getValue(),
             // cursores
             cSetup: cSetup,
             cDraw: cDraw,
             cAux: cAux,
-            nodeName: Dp5.nodeName
+            nodeName: Lp5.nodeName
       }
       sio.emit('liveleparc1', o)
 }
